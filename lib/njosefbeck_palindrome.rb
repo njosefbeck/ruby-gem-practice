775f552951785129df973370c80834f6b487c28e
Nathan Beck <njosefbeck@gmail.com>
2020-05-15 16:20:36 -0500

    Initialize repo

---
+++ lib/njosefbeck_palindrome.rb
@@ -0,0 +1,6 @@
+require "njosefbeck_palindrome/version"
+
+module NjosefbeckPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/njosefbeck_palindrome/version.rb
@@ -0,0 +1,3 @@
+module NjosefbeckPalindrome
+  VERSION = "0.1.0"
+end
